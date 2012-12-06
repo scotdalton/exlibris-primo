21a172db3e28f64e618c706f1cf55e79a98b003a
Scot Dalton <scotdalton@gmail.com>
2012-12-06 16:19:07 -0500

    Remove insufficient query method. Not for this gem to determine.

--- lib/exlibris/primo/search.rb
+++ lib/exlibris/primo/search.rb
@@ -2,22 +2,22 @@ module Exlibris
   module Primo
     #
     # Search Primo using Exlibris::Primo::Search
-    # 
+    #
     #     search = Search.new.base_url!("http://primo.library.edu").
     #       add_query_term("Digital divide", "any", "contains")
     #     search.records #=> Array of Primo records
     #     search.facets #=> Array of Primo facets
     #     search.size #=> Total number of records in the search
-    # 
+    #
     # There are a several configuration setting for the search
-    #  
+    #
     #     search = Search.new.base_url!("http://primo.library.edu").institution!("PRIMO").
     #       enable_did_u_mean.on_campus.page_size!(20).start_index!(21).add_sort_by("stitle")
     #         add_query_term("Digital divide", "any", "contains")
     #     search.records #=> Array of Primo records
     #     search.facets #=> Array of Primo facets
     #     search.size #=> Total number of records in the search
-    # 
+    #
     class Search
       include Config::Attributes
       include ChainGang::Base
@@ -26,55 +26,42 @@ module Exlibris
       include RequestAttributes
       include WriteAttributes
 
-      # 
+      #
       # Returns the Response from the search.
       # Not really intended for public consumption.
-      # 
+      #
       def search
         request.write_attributes request_attributes
         @search ||= request.call
       end
 
-      # 
+      #
       # Returns the array of Records from the search.
-      # 
+      #
       def records
         @records ||= search.records
       end
 
-      # 
+      #
       # Returns the array of Facets from the search.
-      # 
+      #
       def facets
         @facets ||= search.facets
       end
 
-      # 
+      #
       # Returns the total number of records from the search
-      # 
+      #
       def size
         @size ||= search.size
       end
 
-      # 
+      #
       # Returns "Did U Mean" suggestion for the search.
-      # 
+      #
       def did_u_mean
         @did_u_mean ||= search.did_u_mean
       end
-
-      # Determine whether we have sufficient search criteria to search
-      # Sufficient means either:
-      #     * We have a Primo doc id
-      #     * We have either an isbn OR an issn
-      #     * We have a title AND an author AND a genre
-      def insufficient_query?
-        return false unless (record_id.nil? or record_id.empty?)
-        return false unless (isbn.nil? or isbn.empty?)
-        return false unless (title.nil? or title.empty?) or (author.nil? or author.empty?) or (genre.nil? or genre.empty?)
-        return true
-      end
-      private :insufficient_query?
 
       def full_view_request
         @full_view_request ||= Exlibris::Primo::WebService::Request::FullView.new
