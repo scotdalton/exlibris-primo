14de3959b9cddf6b909d5baa333f04306f731a4c
Scot Dalton <scotdalton@gmail.com>
2012-12-20 15:35:44 -0500

    Update error handling.

--- lib/exlibris/primo/web_service/response/base/error.rb
+++ lib/exlibris/primo/web_service/response/base/error.rb
@@ -7,12 +7,16 @@ module Exlibris
             (error_code && (not error_code.eql? "0"))
           end
 
+          def error
+            @error ||= xml_without_namespaces.root.at_xpath("//ERROR")
+          end
+
           def error_code
-            @error_code ||= xml_without_namespaces.root.at_xpath("//ERROR")["CODE"]
+            @error_code ||= error["CODE"] if error
           end
 
           def error_message
-            @error_message ||= xml_without_namespaces.root.at_xpath("//ERROR")["MESSAGE"]
+            @error_message ||= error["MESSAGE"] if error
           end
         end
       end
