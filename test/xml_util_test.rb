2cf2d0dd011113cba4b7967850d420cb1475b770
Jaron Kennel <jaronkk@gmail.com>
2013-04-10 15:56:37 -0400

    Fix for parsing records with the prim namespace prefix.

--- test/xml_util_test.rb
+++ test/xml_util_test.rb
@@ -20,4 +20,25 @@ class XmlUtilTest < Test::Unit::TestCase
       assert_equal "{\"record\":{\"control\":{\"recordid\":\"123456\"}}}", record.to_json
     }
   end
+
+  def test_primo_central_record_to_xml
+    assert_nothing_raised {
+      record = Exlibris::Primo::Record.new(:raw_xml => "<prim:record><prim:control><prim:recordid>123456</prim:recordid></prim:control></prim:record>\n", :namespaces => {"xmlns:prim" => "http://www.exlibrisgroup.com/xsd/primo/primo_nm_bib"})
+      assert_equal "<record><control><recordid>123456</recordid></control></record>", record.to_xml
+    }
+  end
+
+  def test_primo_central_record_to_hash
+    assert_nothing_raised {
+      record = Exlibris::Primo::Record.new(:raw_xml => "<prim:record><prim:control><prim:recordid>123456</prim:recordid></prim:control></prim:record>\n", :namespaces => {"xmlns:prim" => "http://www.exlibrisgroup.com/xsd/primo/primo_nm_bib"})
+      assert_equal({"record"=> { "control" => { "recordid"=> "123456" } } }, record.to_hash)
+    }
+  end
+
+  def test_primo_central_record_to_json
+    assert_nothing_raised {
+      record = Exlibris::Primo::Record.new(:raw_xml => "<prim:record><prim:control><prim:recordid>123456</prim:recordid></prim:control></prim:record>\n", :namespaces => {"xmlns:prim" => "http://www.exlibrisgroup.com/xsd/primo/primo_nm_bib"})
+      assert_equal "{\"record\":{\"control\":{\"recordid\":\"123456\"}}}", record.to_json
+    }
+  end
 end
