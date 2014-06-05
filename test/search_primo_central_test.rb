ec792aff22a7cd7ffa0cd91ae46f238572352fef
Jaron Kennel <jaronkk@gmail.com>
2014-06-05 12:55:14 -0400

    Broken test for the MLA source with image

--- test/search_primo_central_test.rb
+++ test/search_primo_central_test.rb
@@ -4,6 +4,7 @@ class SearchPrimoCentralTest < Test::Unit::TestCase
     @base_url = "http://primo-fe1.library.nd.edu:1701"
     @institution = "NDU"
     @search_term = "van gogh"
+    @mla_doc_id = "mla2012444463"
   end
 
   def test_primo_central
@@ -20,6 +21,23 @@ class SearchPrimoCentralTest < Test::Unit::TestCase
         assert_not_nil record.tables_of_contents
         assert_not_nil record.related_links
       end
+    end
+  end
+
+  def test_mla_source
+    VCR.use_cassette('search primo central mla') do
+      search = Exlibris::Primo::Search.new.base_url!(@base_url).
+        institution!(@institution).add_adaptor_location('primo_central_multiple_fe').add_query_term(@mla_doc_id, "rid", "exact").on_campus
+      assert_not_nil search.records
+      assert((not search.records.empty?))
+      assert_not_nil search.size
+      record = search.records.first
+      assert record.respond_to?(:display_title), "Record should have a display title"
+      assert_not_nil record.display_title
+      assert_not_nil record.fulltexts
+      assert_not_nil record.tables_of_contents
+      assert_not_nil record.related_links
+      assert_equal record.display_source, "MLA International Bibliography<img src=\"http://exlibris-pub.s3.amazonaws.com/mlalogo_001.jpg\" style=\"vertical-align:middle;margin-left:7px\">"
     end
   end
 end
